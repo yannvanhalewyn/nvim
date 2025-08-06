9f63a6a79924e419243b57064b5119c616382c9a
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2025-08-06 03:05:56 +0200

    Add some bindings and options

--- init.lua
+++ init.lua
@@ -1,13 +1,38 @@
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.wrap = false
+vim.o.signcolumn = "yes"
+vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 vim.o.swapfile = false
+vim.o.winborder = "rounded"
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>so", ":update<CR> :source<CR>")
 vim.keymap.set("n", "<leader>fs", ":write<CR>")
 vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>hh", ":Pick help<CR>")
+vim.keymap.set("n", "<leader>bb", ":Pick buffers<CR>")
+vim.keymap.set("n", "<leader>bd", ":bdelete<CR>")
+vim.keymap.set("n", "<leader> ", ":Pick files<CR>")
+vim.keymap.set("n", "<leader>d", ":Oil<CR>")
+
+vim.keymap.set("n", "[e", function()
+	vim.diagnostic.jump({ count = -1, float = true })
+end)
+vim.keymap.set("n", "]e", function()
+	vim.diagnostic.jump({ count = 1, float = true })
+end)
+vim.keymap.set("n", "<leader>ce", vim.diagnostic.open_float)
+
+-- Window
+vim.keymap.set("n", "<leader>wv", ":vsplit<CR><C-w>l")
+vim.keymap.set("n", "<leader>ws", ":split<CR><C-w>j")
+vim.keymap.set("n", "<leader>wq", ":quit<CR>")
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
 
 vim.pack.add({
 	{ src = "https://github.com/vague2k/vague.nvim" },
@@ -17,13 +42,27 @@ vim.pack.add({
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 })
 
+require("mini.pick").setup()
+require("oil").setup()
+
 require('nvim-treesitter.configs').setup({
 	highlight = {
 		enable = true,
 	},
 })
 
-vim.lsp.enable({ "lua_ls" })
+vim.lsp.enable({ "lua_ls", "clojure_lsp" })
+
+-- Sets up omnicomplete with LSP
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(event)
+		local client = vim.lsp.get_client_by_id(event.data.client_id);
+		if client:supports_method('textDocument/completion') then
+			vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
+		end
+	end
+})
+vim.cmd("set completeopt+=noselect")
 
 vim.cmd("colorscheme vague")
 vim.cmd(":hi statusline guibg=NONE")
