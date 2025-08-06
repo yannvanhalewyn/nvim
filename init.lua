b2b25aee3a6ac2bae06eedd00878f17d1a1b0a0d
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2025-08-06 02:21:10 +0200

    Setup very basic init.lua

---
+++ init.lua
@@ -0,0 +1,22 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.wrap = false
+vim.o.tabstop = 4
+vim.o.swapfile = false
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>so", ":update<CR> :source<CR>")
+vim.keymap.set("n", "<leader>fs", ":write<CR>")
+vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format)
+
+vim.pack.add({
+		{src = "https://github.com/vague2k/vague.nvim"},
+		{src = "https://github.com/stevearc/oil.nvim"},
+		{src = "https://github.com/echasnovski/mini.pick"},
+		{src = "https://github.com/neovim/nvim-lspconfig"},
+})
+
+vim.lsp.enable({"lua_ls"})
+
+vim.cmd("colorscheme vague")
+vim.cmd(":hi statusline guibg=NONE")
