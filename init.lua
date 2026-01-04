0c23e2efef94f4508de128306eafdd23a91b56fe
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2026-01-04 09:25:42 +0100

    (feat): re-enable new dialog box

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@
 -- Options
 -- Enable experimental message box: https://github.com/neovim/neovim/pull/27855
 -- Disabled because it breaks vim.pack install
--- require('vim._extui').enable({})
+require('vim._extui').enable({})
 
 vim.o.number = true
 vim.o.relativenumber = true
